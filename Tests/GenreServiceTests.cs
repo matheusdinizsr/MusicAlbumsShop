a55678e7d4f8959b9149c4d8aa92cc37be3f3e6f
Matheus Diniz <matheusdinizsr@gmail.com>
2024-10-23 20:40:19 -0300

    Fixed GenreServiceTests. Added the context with the connection string.

--- Tests/GenreServiceTests.cs
+++ Tests/GenreServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using MusicAlbumsShop.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAlbumsShop.Models;
 using MusicAlbumsShop.Services;
 using MusicAlbumsShop.Storage;
 using System;
@@ -9,43 +10,25 @@ using System.Threading.Tasks;
 
 namespace Tests
 {
-    internal class FakeGenreStorage : IGenreStorage
-    {
-        public Genre AddGenre(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Genre? GetGenre(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Genre? GetGenreById(int genreId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Genre[] GetGenres()
-        {
-            var genres = new Genre[]
-            { 
-                new Genre { Id = 1, Name = "Rock" },
-                new Genre { Id = 2, Name = "Pop" }
-            };
-
-            return genres;
-        }
-    }
+    
     internal class GenreServiceTests
     {
         private GenreService _service;
-        private FakeGenreStorage _storage;
+        private GenreStorage _storage;
+        private MusicAlbumsContext context;
+        private MusicAlbumsContext _testContext;
+        private DbContextOptions<MusicAlbumsContext> _dbContextOptions;
 
         [SetUp]
         public void Setup()
         {
-            _storage = new FakeGenreStorage();
+            var builder = new DbContextOptionsBuilder<MusicAlbumsContext>();
+            builder.UseSqlServer("Data Source=Math;Initial Catalog=testmusicalbumsshop;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            _dbContextOptions = builder.Options;
+            context = new MusicAlbumsContext(_dbContextOptions);
+            _storage = new GenreStorage(context);
+            _testContext = new MusicAlbumsContext(_dbContextOptions);
+            context.Database.EnsureCreated();
             _service = new GenreService(_storage);
         }
 
@@ -53,15 +36,28 @@ namespace Tests
         public void When_GetGenres_Success()
         {
             // arrange
+            var rock = _storage.AddGenre("Rock");
+            var blues = _storage.AddGenre("Blues");
+            var counter = _testContext.Genres.Count();
 
             // act
             var response = _service.GetGenres();
 
             // assert
             Assert.That(response, Is.Not.Null);
-            Assert.That(response.Genres.Count, Is.EqualTo(2));
+            Assert.That(response.Genres.Count(), Is.EqualTo(counter));
             Assert.That(response.Genres.Contains("Rock"), Is.True);
-            Assert.That(response.Genres.Contains("Pop"), Is.True);
+            Assert.That(response.Genres.Contains("Blues"), Is.True);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Dispose();
+                _testContext.Dispose();
+            }
         }
     }
 }
